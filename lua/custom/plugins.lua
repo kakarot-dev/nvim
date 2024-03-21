2ce808eef59f29967f20c3e81469ad99bfd3d980
Joel <kakarot.joel@gmail.com>
2024-03-21 13:07:38 +0530

    remove useless modules

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -65,13 +65,6 @@ local plugins = {
     end,
   },
   {
-    "jose-elias-alvarez/null-ls.nvim",
-    event = "VeryLazy",
-    opts = function()
-      return require "custom.configs.null-ls"
-    end,
-  },
-  {
     "neovim/nvim-lspconfig",
     config = function()
       require "plugins.configs.lspconfig"
